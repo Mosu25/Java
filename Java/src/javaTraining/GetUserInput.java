b0b07eb4b2548856adef3a77bd11a5ec3fcee938
Mosu25 <mosu.avms@gmail.com>
2022-09-05 23:28:50 +0530

    Adding Get user input concept

---
+++ Java/src/javaTraining/GetUserInput.java
@@ -0,0 +1,31 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class GetUserInput {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); //Scanner object creation
+		System.out.println("Enter your Name");
+		String name= sc.nextLine(); //Read user input
+		System.out.println("User Name: "+name); // output -- Display user input
+		
+		/*
+		 
+		nextBoolean()	Reads a boolean value from the user
+		nextByte()		Reads a byte value from the user
+		nextDouble()	Reads a double value from the user
+		nextFloat()		Reads a float value from the user
+		nextInt()		Reads a int value from the user
+		nextLine()		Reads a String value from the user
+		nextLong()		Reads a long value from the user
+		nextShort()		Reads a short value from the user
+		
+		*/
+		
+		
+		
+	}
+
+}
